f70323e0f3c2ab7d0fd31ff4554ccd75f1fbe771
Simon Szalai <simon.szalai@gmail.com>
2022-12-31 16:31:56 -0300

    add psycopg2

--- setup.py
+++ setup.py
@@ -3,6 +3,18 @@ from setuptools import find_packages, setup
 setup(
     name="pde_dagster",
     packages=find_packages(exclude=["pde_dagster_tests"]),
-    install_requires=["dagster", "dagster-cloud", "boto3", "pandas", "matplotlib", "textblob", "tweepy", "wordcloud", "pandas_datareader", "yfinance"],
+    install_requires=[
+        "dagster",
+        "dagster-cloud",
+        "boto3",
+        "pandas",
+        "matplotlib",
+        "textblob",
+        "tweepy",
+        "wordcloud",
+        "pandas_datareader",
+        "yfinance",
+        "psycopg2-binary",
+    ],
     extras_require={"dev": ["dagit", "pytest"]},
 )
