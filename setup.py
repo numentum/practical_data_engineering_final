7e442acf5e612e61ee67cefe6c1c67720d6d561f
Simon Szalai <simon.szalai@gmail.com>
2022-12-31 16:29:11 -0300

    add yfinance to deps

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import find_packages, setup
 setup(
     name="pde_dagster",
     packages=find_packages(exclude=["pde_dagster_tests"]),
-    install_requires=["dagster", "dagster-cloud", "boto3", "pandas", "matplotlib", "textblob", "tweepy", "wordcloud", "pandas_datareader"],
+    install_requires=["dagster", "dagster-cloud", "boto3", "pandas", "matplotlib", "textblob", "tweepy", "wordcloud", "pandas_datareader", "yfinance"],
     extras_require={"dev": ["dagit", "pytest"]},
 )
