73857374f14d0749305532afcfd967204bc21e96
Simon Szalai <simon.szalai@gmail.com>
2022-12-31 16:25:56 -0300

    add pandas_datareader to deps

--- setup.py
+++ setup.py
@@ -3,15 +3,6 @@ from setuptools import find_packages, setup
 setup(
     name="pde_dagster",
     packages=find_packages(exclude=["pde_dagster_tests"]),
-    install_requires=[
-        "dagster",
-        "dagster-cloud",
-        "boto3",
-        "pandas",
-        "matplotlib",
-        "textblob",
-        "tweepy",
-        "wordcloud",
-    ],
+    install_requires=["dagster", "dagster-cloud", "boto3", "pandas", "matplotlib", "textblob", "tweepy", "wordcloud", "pandas_datareader"],
     extras_require={"dev": ["dagit", "pytest"]},
 )
