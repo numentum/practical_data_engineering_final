35f8cc1a809275c0f188361d62105c79118d999a
Simon Szalai <simon.szalai@gmail.com>
2023-01-10 19:27:16 -0300

    remove executor

--- pde_dagster/jobs/J5_process_files.py
+++ pde_dagster/jobs/J5_process_files.py
@@ -6,7 +6,7 @@ from pde_dagster.ops.load_table import load_table_op
 from pde_dagster.resources.postgres import postgres_resource
 
 
-@job(resource_defs={"postgres_resource": postgres_resource}, executor_def=in_process_executor)
+@job(resource_defs={"postgres_resource": postgres_resource})
 def J5_process_files():
     df = parallel_load_files_op()
     df = transform_market_transactions_op(df)
