280077262603aefcbd37c7a30d7a22252976eed6
Simon Szalai <simon.szalai@gmail.com>
2023-01-10 19:48:38 -0300

    back to parallel

--- pde_dagster/jobs/J5_process_files.py
+++ pde_dagster/jobs/J5_process_files.py
@@ -1,15 +1,15 @@
 from dagster import job
 
-from pde_dagster.ops.J5_process_files.load_files import load_files_op
-
-# from pde_dagster.ops.J5_process_files.parallel_load_files import parallel_load_files_op
+from pde_dagster.ops.J5_process_files.parallel_load_files import parallel_load_files_op
 from pde_dagster.ops.J5_process_files.transform_market_transactions import transform_market_transactions_op
 from pde_dagster.ops.load_table import load_table_op
 from pde_dagster.resources.postgres import postgres_resource
 
 
-@job(resource_defs={"postgres_resource": postgres_resource})
+@job(
+    resource_defs={"postgres_resource": postgres_resource},
+)
 def J5_process_files():
-    df = load_files_op()
+    df = parallel_load_files_op()
     df = transform_market_transactions_op(df)
     load_table_op(df)
