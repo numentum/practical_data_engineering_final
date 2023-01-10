480ead5e9eab0d2ad9f804a6b734bbb99929f2b9
Simon Szalai <simon.szalai@gmail.com>
2023-01-10 19:50:49 -0300

    delete load files

--- pde_dagster/ops/J5_process_files/load_files.py
+++
@@ -1,9 +0,0 @@
-from dagster import op
-from practical_data_engineering_shared.io.drive import load_files_to_df
-
-
-@op(name="load_files_op")
-def load_files_op(context):
-    df = load_files_to_df(verbose=True, context=context)
-
-    return df
