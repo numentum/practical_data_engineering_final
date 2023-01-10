535b90ec4e13e84a7eabac150eb1d5470e2ddb6a
Simon Szalai <simon.szalai@gmail.com>
2023-01-10 19:34:34 -0300

    change to linear load

--- pde_dagster/jobs/J5_process_files.py
+++ pde_dagster/jobs/J5_process_files.py
@@ -1,6 +1,8 @@
-from dagster import job, in_process_executor
+from dagster import job
 
-from pde_dagster.ops.J5_process_files.parallel_load_files import parallel_load_files_op
+from pde_dagster.ops.J5_process_files.load_files import load_files_op
+
+# from pde_dagster.ops.J5_process_files.parallel_load_files import parallel_load_files_op
 from pde_dagster.ops.J5_process_files.transform_market_transactions import transform_market_transactions_op
 from pde_dagster.ops.load_table import load_table_op
 from pde_dagster.resources.postgres import postgres_resource
@@ -8,6 +10,6 @@ from pde_dagster.resources.postgres import postgres_resource
 
 @job(resource_defs={"postgres_resource": postgres_resource})
 def J5_process_files():
-    df = parallel_load_files_op()
+    df = load_files_op()
     df = transform_market_transactions_op(df)
     load_table_op(df)
