ef50ac5af8211fa678ec2a3484635d9d7bc4caf5
Simon Szalai <simon.szalai@gmail.com>
2023-01-10 16:56:41 -0300

    add logging

--- pde_dagster/ops/J5_process_files/parallel_load_files.py
+++ pde_dagster/ops/J5_process_files/parallel_load_files.py
@@ -4,6 +4,6 @@ from practical_data_engineering_shared.io.drive import parallel_load_files_to_df
 
 @op(name="parallel_load_files_op")
 def parallel_load_files_op(context):
-    df = parallel_load_files_to_df()
+    df = parallel_load_files_to_df(context=context)
 
     return df
